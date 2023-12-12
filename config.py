73f8e7ffe7cca9db16cac660d98a0f8f773872bb
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-13 01:46:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID=
-    API_HASH=""
-    TOKEN=""
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "").split(" "))
-    START_IMG="jdjsjw"
-    BOT_ID=
-    BOT_USERNAME=""
-    BOT_NAME=""
+    API_ID=24509589
+    API_HASH="717cf21d94c4934bcbe1eaa1ad86ae75"
+    TOKEN="6796545941:AAGyuhnW9Gi4nCbyUsrB-SzbqUHArgtHVXg"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "6691393517").split(" "))
+    START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
+    BOT_ID=6796545941
+    BOT_USERNAME="ZuliaiBot"
+    BOT_NAME="ZuliaiBot"
