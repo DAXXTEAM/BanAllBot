f63faa6dc45ba6063a15df24d33150ee5c1207ed
DAXXTEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-12-13 11:48:55 +0530

    Update __init__.py

--- BanAllBot/__init__.py
+++ BanAllBot/__init__.py
@@ -42,7 +42,7 @@ app=Client(
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=TOKEN,
-    plugins=dict(root="BanAllBot/modules")
+    plugins=dict(root="BanAllBot.modules")
      )
 
 LOG.info("starting the bot....")
